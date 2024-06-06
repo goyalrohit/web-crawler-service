0980c4472c26ea85b8d7be5b2cd0ec27ee7cbde3
Rohit Goyal <irohitgoyal@gmail.com>
2024-06-06 23:09:29 +0530

    Web Client Server Refactor

--- src/main/java/com/rohit/app/webcrawlerservice/jms/producer/ClientNotificationAmqProducer.java
+++ src/main/java/com/rohit/app/webcrawlerservice/jms/producer/ClientNotificationAmqProducer.java
@@ -5,11 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Component;
 
-import jakarta.jms.JMSException;
-import jakarta.jms.Message;
 import java.util.UUID;
 
 @Log4j2
